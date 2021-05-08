13747408d8556405b973463db9029436be580c88
yuler <joyful4yu@gmail.com>
2021-05-08 19:00:13 +0800

    Add `hashCode` function

---
+++ src/string/hashCode.ts
@@ -0,0 +1,18 @@
+/**
+ * Javascript implementation of Java’s String.hashCode() method
+ * refs: https://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method
+ * refs: https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#hashCode--
+ *
+ * Returns a hash code for a string.
+ * @param str string
+ * @returns number
+ */
+export function hashCode(string: string) {
+	let hash = 0;
+	for (let i = 0; i < string.length; i++) {
+		hash = (hash << 5) - hash + string.charCodeAt(i);
+		hash = Math.trunc(hash); // Convert to 32bit integer
+	}
+
+	return hash;
+}
---
+++ src/string/index.ts
@@ -0,0 +1,1 @@
+export * from './hashCode.js';
