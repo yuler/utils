ae15deca1236eb960a62de9e7bf01243a00e71f3
yuler <joyful4yu@gmail.com>
2021-06-21 19:05:49 +0800

    wip: Work in progress [skip ci]

--- src/is/index.ts
+++ src/is/index.ts
@@ -1,1 +1,31 @@
-export * from './regexp.js';
+const objectTypeNames = [
+    'Function',
+
+] as const
+
+type ObjectTypeName = typeof objectTypeNames[number]
+
+const isObejectTypeName = (name: unknown): name is ObjectTypeName => {
+    return objectTypeNames.includes(name as ObjectTypeName);
+}
+
+const {toString} = Object.prototype
+const getObjectType = (value: unknown): ObjectTypeName | undefined => {
+    const objectTypeName = toString.call(value).slice(8, -1)
+
+    if (isObejectTypeName(objectTypeName)) {
+        return objectTypeName
+    }
+
+    return undefined
+}
+
+/**
+ * Check if a value is a regular expression.
+ *
+ * @param input - value to check
+ * @returns boolean
+ */
+export function isRegexp(input: unknown): input is RegExp {
+	return Object.prototype.toString.call(input) === '[object RegExp]';
+}
