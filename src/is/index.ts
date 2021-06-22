319499fbc8decb42e30db4bc792cae13821ff528
yuler <joyful4yu@gmail.com>
2021-06-22 10:48:45 +0800

    Add `isUndefined` function

--- src/is/index.ts
+++ src/is/index.ts
@@ -1,23 +1,8 @@
-const objectTypeNames = [
-    'Function',
-
-] as const
-
-type ObjectTypeName = typeof objectTypeNames[number]
-
-const isObejectTypeName = (name: unknown): name is ObjectTypeName => {
-    return objectTypeNames.includes(name as ObjectTypeName);
-}
-
-const {toString} = Object.prototype
-const getObjectType = (value: unknown): ObjectTypeName | undefined => {
-    const objectTypeName = toString.call(value).slice(8, -1)
-
-    if (isObejectTypeName(objectTypeName)) {
-        return objectTypeName
-    }
-
-    return undefined
+/**
+ * Check if a value is undefined.
+ */
+export function isUndefined(input: unknown): input is undefined {
+	return typeof input === undefined;
 }
 
 /**
